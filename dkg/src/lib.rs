e1dbf4d826d34588c9f642d32567bffd90fba463
448 OG <superuser@448.africa>
2026-04-11 12:36:56 +0300

    Implement FrostCredential
    `FrostCredential` contains the details describing a frost credential
    - its seed
    - its byte representation
    - its `FrostCrdentialType`
    
    This commit also adds `Into` conversions for errors for the
    current FROST operations and email validity checks

---
+++ dkg/src/lib.rs
@@ -0,0 +1,8 @@
+mod types;
+pub use types::*;
+
+mod random;
+pub use random::*;
+
+mod errors;
+pub use errors::*;
---
+++ dkg/src/random.rs
@@ -0,0 +1,36 @@
+use rand_chacha::{
+    ChaCha12Rng,
+    rand_core::{RngCore, SeedableRng},
+};
+use subtle::ConstantTimeEq;
+use zeroize::Zeroizing;
+
+pub struct RandomBytes<const N: usize>(Zeroizing<[u8; N]>);
+
+impl<const N: usize> RandomBytes<N> {
+    pub fn generate() -> Self {
+        let mut rng = ChaCha12Rng::from_os_rng();
+
+        let mut buffer = Zeroizing::new([0u8; N]);
+
+        rng.fill_bytes(buffer.as_mut());
+
+        Self(buffer)
+    }
+
+    pub fn expose(&self) -> &[u8; N] {
+        &self.0
+    }
+
+    pub fn take(self) -> Zeroizing<[u8; N]> {
+        self.0
+    }
+
+    pub fn const_cmp(&self, other: &RandomBytes<N>) -> bool {
+        self.expose().ct_eq(other.expose()).into()
+    }
+
+    pub fn const_cmp_bytes(&self, other: &[u8; N]) -> bool {
+        self.expose().ct_eq(other.as_slice()).into()
+    }
+}
